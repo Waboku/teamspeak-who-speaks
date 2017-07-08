d96215213bf6af6d8130fab1ce5868d146cdf08d
Vito Kortbeek <vito.kortbeek@gmail.com>
2017-07-09 00:16:16 +0200

    Adds a script that converts space tabs to hard tabs
    Excludes the ASF directory in src (./src/ASF)
    Other excludes can be added:
    -type d \( -path ./src/ASF -o -path ./other/path \)
    
    The file is indented with spaces ;)

---
+++ Tools/space2tabs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+unexpand_func () {
+    echo "File: $1"
+    unexpand -t 4 "$1" > "$1.tmp"
+    mv "$1.tmp" "$1"
+}
+
+export -f unexpand_func
+
+find . -type d \( -path ./src/ASF \) -prune -o -name "*.[ch]" -exec bash -c 'unexpand_func {}' \;
