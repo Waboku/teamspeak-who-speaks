309689e800a1faa0a8bd54844f5e1670be4eae75
waboku <aran_dokoupil@hotmail.com>
2018-06-10 16:25:37 +0200

    Edited the graphics file st7775 to have a flipped define, so when the display decides to mirror it can easily be fixed..
    Added some assets

--- src/main.c
+++ src/main.c
@@ -20,9 +20,36 @@
 #include "graphics.h"
 #include "text.h"
 #include "gui/gui_list.h"
+#include "tetris.h"
+#include "fonts/Arial.h"
+#include "bitmaps/all_bitmaps.h"
 
 #define RAND_POS (screen_pos_t){rand()%LCD_X_SIZE,rand()%LCD_Y_SIZE}
 #define RAND_COLOR (RGBcolor_t){rand()%255,rand()%255,rand()%255}
+	
+	
+	
+	
+struct usart_module usart_instance;
+
+void uart_init(void){
+	
+	struct usart_config config_usart;
+	usart_get_config_defaults(&config_usart);
+	
+	config_usart.baudrate    = 57600;
+	config_usart.mux_setting = USART_RX_1_TX_0_XCK_1;
+	config_usart.pinmux_pad0 = PINMUX_PA18C_SERCOM1_PAD2;
+	config_usart.pinmux_pad1 = PINMUX_PA19C_SERCOM1_PAD3;
+	config_usart.pinmux_pad2 = PINMUX_UNUSED;
+	config_usart.pinmux_pad3 = PINMUX_UNUSED;
+	
+	while (usart_init(&usart_instance, ESP_SERCOM, &config_usart) != STATUS_OK) {
+		
+	}
+	
+	usart_enable(&usart_instance);
+}
 
 
 static void pin_init(void)
@@ -51,13 +78,22 @@ int main(void)
 	
 	st7775_init();
 	
+	uart_init();
+	
 	
 	printf("Starting..\n");	
 	
 	graphics_drawSolidRectangle((screen_pos_t){0,0},(screen_pos_t){LCD_X_MAX,LCD_Y_MAX},WHITE);
+	graphics_drawSolidRectangle(RAND_POS,RAND_POS,YELLOW);	
+	graphics_drawBitmap((screen_pos_t){10,10},&BIG_F);
 	
 	
-	while (true) {
-		graphics_drawSolidRectangle(RAND_POS,RAND_POS,RAND_COLOR);
+	while (true)
+	{
+		for(uint8_t x = LCD_X_MAX; x>=10; x--){
+			text_RenderWordWrapedString((screen_pos_t){5,100},(screen_pos_t){LCD_Y_MAX,x},"The quick brown fox jumps over the lazy dog",&arial,BLACK);
+		}
 	}
+	
 }
+
---
+++ src/uart.c
@@ -0,0 +1,12 @@
+/*
+ * uart.c
+ *
+ * Created: 16-10-2017 12:35:59
+ *  Author: P
+ */ 
+
+#include "asf.h"
+#include "uart.h"
+#include <stdio.h>
+
+
